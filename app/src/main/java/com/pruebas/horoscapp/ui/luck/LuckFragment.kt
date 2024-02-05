c5049bf85544925ce49b6a995f2e06d1183137ad
Lenovo <daminu200@gmail.com>
2024-02-05 19:41:53 +0100

    Pantalla luck acabada

--- app/src/main/java/com/pruebas/horoscapp/ui/luck/LuckFragment.kt
+++ app/src/main/java/com/pruebas/horoscapp/ui/luck/LuckFragment.kt
@@ -1,6 +1,7 @@
 package com.pruebas.horoscapp.ui.luck
 
 import android.animation.ObjectAnimator
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -16,9 +17,12 @@ import androidx.fragment.app.viewModels
 import com.pruebas.horoscapp.R
 import com.pruebas.horoscapp.databinding.FragmentHoroscopeBinding
 import com.pruebas.horoscapp.databinding.FragmentLuckBinding
+import com.pruebas.horoscapp.ui.core.listeners.OnSwipeTouchListener
 import com.pruebas.horoscapp.ui.horoscope.HoroscopeViewModel
+import com.pruebas.horoscapp.ui.providers.RandomCardProvider
 import dagger.hilt.android.AndroidEntryPoint
 import java.util.Random
+import javax.inject.Inject
 
 @AndroidEntryPoint
 
@@ -28,17 +32,52 @@ class LuckFragment : Fragment() {
     private var _binding: FragmentLuckBinding? = null
     private val binding get() = _binding!!
 
+    @Inject
+    lateinit var randomCardProvider: RandomCardProvider
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initUI()
     }
 
     private fun initUI() {
+        preparePrediction()
         initListeners()
     }
 
+    private fun preparePrediction() {
+        val currentLuck = randomCardProvider.getLucky()
+        currentLuck?.let { luck ->
+            val currentPrediction = getString(luck.text)
+            binding.tvLucky.text = currentPrediction
+            binding.ivLuckyCard.setImageResource(luck.image)
+            binding.tvShare.setOnClickListener { shareResult(currentPrediction) }
+        }
+    }
+
+    private fun shareResult(prediction: String) {
+        val sendIntent: Intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, prediction)
+            type = "text/plain"
+        }
+
+        val shareIntent = Intent.createChooser(sendIntent, null)
+        startActivity(shareIntent)
+    }
+
     private fun initListeners() {
-        binding.ivRoulette.setOnClickListener { spinRoulette() }
+        /*
+                binding.ivRoulette.setOnClickListener { spinRoulette() }
+        */
+        binding.ivRoulette.setOnTouchListener(object : OnSwipeTouchListener(requireContext()) {
+            override fun onSwipeRight() {
+                spinRoulette()
+            }
+            override fun onSwipeLeft() {
+                spinRoulette()
+            }
+        })
     }
 
     private fun spinRoulette() {
@@ -76,7 +115,7 @@ class LuckFragment : Fragment() {
     private fun growCard() {
         val growAnimation = AnimationUtils.loadAnimation(requireContext(), R.anim.grow)
 
-        growAnimation.setAnimationListener(object : Animation.AnimationListener{
+        growAnimation.setAnimationListener(object : Animation.AnimationListener {
             override fun onAnimationStart(p0: Animation?) {}
 
             override fun onAnimationEnd(p0: Animation?) {
@@ -94,27 +133,27 @@ class LuckFragment : Fragment() {
 
     private fun showPremonitionView() {
 
-                val disappearAnimation = AlphaAnimation(1.0f, 0.0f)
-                disappearAnimation.duration = 200
+        val disappearAnimation = AlphaAnimation(1.0f, 0.0f)
+        disappearAnimation.duration = 200
 
-                val appearAnimation = AlphaAnimation(0.0f, 1.0f)
-                appearAnimation.duration = 1000
+        val appearAnimation = AlphaAnimation(0.0f, 1.0f)
+        appearAnimation.duration = 1000
 
-                disappearAnimation.setAnimationListener(object : Animation.AnimationListener{
-                    override fun onAnimationStart(p0: Animation?) {}
+        disappearAnimation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(p0: Animation?) {}
 
-                    override fun onAnimationEnd(p0: Animation?) {
-                        binding.preview.isVisible = false
-                        binding.prediction.isVisible = true
-                    }
+            override fun onAnimationEnd(p0: Animation?) {
+                binding.preview.isVisible = false
+                binding.prediction.isVisible = true
+            }
 
-                    override fun onAnimationRepeat(p0: Animation?) {}
+            override fun onAnimationRepeat(p0: Animation?) {}
 
-                })
+        })
 
 
-                binding.preview.startAnimation(disappearAnimation)
-                binding.prediction.startAnimation(appearAnimation)
+        binding.preview.startAnimation(disappearAnimation)
+        binding.prediction.startAnimation(appearAnimation)
 
 
     }
